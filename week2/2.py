5102374e53e8e9f26faca211f4bff9868773680f
Ayush Dubey <ayushdubey70@gmail.com>
2020-03-14 14:01:51 +0530

    python: add Program 2 for Week 2
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ week2/2.py
@@ -0,0 +1,25 @@
+"""
+Input Format:
+
+The first line of the input contains two numbers separated by a space.
+
+Output Format:
+
+Print the addition in single line
+
+Example:
+
+Input:
+4 2
+
+Output:
+6
+
+"""
+
+
+x,y = input().split(" ")
+x = int(x)
+y = int(y)
+
+print(x+y)
