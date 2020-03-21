fdc50664e9578c7ba467cdf34b19c08db2e9be69
Ayush Dubey <ayushdubey70@gmail.com>
2020-03-21 13:55:30 +0530

    python: add program 2 for week 4
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

---
+++ week4/2.py
@@ -0,0 +1,28 @@
+"""
+Input Format:
+
+A number n.
+
+Output Format:
+
+Print the factorial of n.
+
+Example:
+
+Input:
+4
+
+Output:
+24
+
+"""
+
+k = int(input())
+
+fac = 1
+for i in range(1,k+1):
+    if(k==0):
+        break
+    fac=fac*i
+
+print(fac)
